07c22d0eb32666d26a4dd2e94eabcd302900aa5a
naomi-eliasar <nreliasar@gmail.com>
2022-06-01 16:30:07 +0200

    feat/details page fetches one villager + information from api, also in redux store

--- src/pages/Details/index.js
+++ src/pages/Details/index.js
@@ -4,32 +4,29 @@ import { useDispatch, useSelector } from "react-redux";
 import { HeroBanner } from "../../components";
 import { selectVillagerDetails } from "../../store/villager/selector";
 import { fetchVillagerDetails } from "../../store/villager/thunk";
+import "./styles.css";
 
 const Details = () => {
   const dispatch = useDispatch();
   const routeParams = useParams();
   const details = useSelector(selectVillagerDetails);
 
-  console.log("details", details);
-
   useEffect(() => {
-    dispatch(fetchVillagerDetails(routeParams.name));
-  }, [dispatch, routeParams.name]);
+    dispatch(fetchVillagerDetails(routeParams.id));
+  }, [dispatch, routeParams.id]);
 
-  // return details ? (
-  //   <div>
-  //     <HeroBanner>
-  //       <h1>{details.name}</h1>
-  //     </HeroBanner>
-  //     <p>{details.id}</p>
-  //   </div>
-  // ) : (
-  //   <p>Loading...</p>
-  // );
-  return (
-    <div>
-      <h1>{details.name}</h1>
+  console.log("route param:", routeParams.id);
+
+  return details ? (
+    <div className="detailPage">
+      <div className="displayDetails">
+        {details.map((detail) => {
+          return <h1>{detail.name}</h1>;
+        })}
+      </div>
     </div>
+  ) : (
+    <p>Loading...</p>
   );
 };
 
